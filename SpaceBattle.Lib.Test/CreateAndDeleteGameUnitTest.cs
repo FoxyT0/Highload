1a94ad066976be042543fef91aa341cd9c291443
Kirill Iost <84028357+FoxyT0@users.noreply.github.com>
2023-05-30 09:23:30 +0600

    Update CreateAndDeleteGameUnitTest.cs

--- SpaceBattle.Lib.Test/CreateAndDeleteGameUnitTest.cs
+++ SpaceBattle.Lib.Test/CreateAndDeleteGameUnitTest.cs
@@ -32,8 +32,8 @@ public class CreateAndDeleteGameUnitTest
 	[Fact]
 	public void GoodCreateTowGamesAndDeleteOneTest()
 	{
-		var gameCommand = IoC.Resolve<Mock<SpaceBattle.Lib.ICommand>>("Game.Create.New", "Game1", "Scope1");
-		var gameCommand2 = IoC.Resolve<Mock<SpaceBattle.Lib.ICommand>>("Game.Create.New", "Game1", "Scope2");
+		var gameCommand = IoC.Resolve<SpaceBattle.Lib.ICommand>("Game.Create.New", "Game1", "Scope1");
+		var gameCommand2 = IoC.Resolve<SpaceBattle.Lib.ICommand>("Game.Create.New", "Game1", "Scope2");
 
 		Assert.True(scopes.Count == 2);
 		Assert.True(scopes["Scope1"] != scopes["Scope2"]);
@@ -47,4 +47,46 @@ public class CreateAndDeleteGameUnitTest
 		Assert.True(scopes.Count == 0);
 	}
 
+	[Fact]
+	public void TestRegisterTimeSpanInGameScope()
+	{
+		var gameCommand = IoC.Resolve<SpaceBattle.Lib.ICommand>("Game.Create.New", "Game1", "Scope1");
+
+		var ts = IoC.Resolve<TimeSpan>("Game.Get.TimeSpan");
+		Assert.True(ts == new TimeSpan(100));
+	}
+
+	[Fact]
+	public void TestRegisterDictOfObjectsInGameScope()
+	{
+		var gameCommand = IoC.Resolve<SpaceBattle.Lib.ICommand>("Game.Create.New", "Game1", "Scope1");
+
+		var obj = IoC.Resolve<IUObject>("Game.Get.Object", "Object1");
+		var objDict = IoC.Resolve<Dictionary<string, IUObject>>("Game.Get.Dict");
+		var testobj = new Mock<IUObject>().Object;
+		Assert.True(objDict.Count == 1);
+		Assert.True(obj.GetType() == testobj.GetType());
+
+		var delFlag = IoC.Resolve<bool>("Game.Delete.Object", "Object1");
+		Assert.True(delFlag == true);
+		Assert.True(objDict.Count == 0);
+		Assert.Throws<KeyNotFoundException>(() => IoC.Resolve<IUObject>("Game.Get.Object", "Object1"));
+	}
+	
+	[Fact]
+	public void TestRegisterQueueInGameScope()
+	{
+		var gameCommand = IoC.Resolve<SpaceBattle.Lib.ICommand>("Game.Create.New", "Game1", "Scope1");
+
+		var fireCommand = new Mock<SpaceBattle.Lib.ICommand>().Object;
+		
+		IoC.Resolve<SpaceBattle.Lib.ICommand>("Queue.Enqueue.Command", fireCommand).Execute();
+		var cmdQueue = IoC.Resolve<Queue<SpaceBattle.Lib.ICommand>>("Game.Get.Queue");
+		Assert.True(cmdQueue.Count == 1);
+
+		SpaceBattle.Lib.ICommand fireCommandE = IoC.Resolve<SpaceBattle.Lib.ICommand>("Queue.Dequeue.Command");
+		Assert.True(fireCommand == fireCommandE);
+		Assert.True(cmdQueue.Count == 0);
+	}
 }
+
