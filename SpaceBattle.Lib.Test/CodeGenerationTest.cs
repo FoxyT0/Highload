9bbf05eb63bf28e69cd5e630228d0dc51b9afb12
Bozhko Daniil <voc.offacc@gmail.com>
2023-06-07 04:32:16 +0600

    Fixed test

--- SpaceBattle.Lib.Test/CodeGenerationTest.cs
+++ SpaceBattle.Lib.Test/CodeGenerationTest.cs
@@ -7,16 +7,7 @@ public class CodeGenerationTest
 	{
 		var ag = new AdapterGenerator(typeof(IMovable));
 		var a = ag.Generate();
-		string r = @$"namespace SpaceBattle.Lib;
-		public class IMovableAdapter:IMovable
-		{{
-			 IUObject target;
-			 public SpaceBattle.Lib.Vector position
-			 {{get => target.get_property(\"position\");set => target.set_property(\"position\", value);}}
-			 public SpaceBattle.Lib.Vector velocity
-			 {{get => target.get_property(\"velocity\");}}
-			 public IMovableAdapter(IUObject target){{this.target=target;}}
-		 }}";
+		string r = $"namespace SpaceBattle.Lib;public class IMovableAdapter:IMovable{{IUObject target;public SpaceBattle.Lib.Vector position{{get => target.get_property(\"position\");set => target.set_property(\"position\", value);}}public SpaceBattle.Lib.Vector velocity{{get => target.get_property(\"velocity\");}}public IMovableAdapter(IUObject target){{this.target=target;}}}}";
 		Assert.Equal(a, r);
 	}
 }
